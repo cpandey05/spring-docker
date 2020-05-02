08ccbdcfa3bb66f14943c18ca046bac4e109d1ed
cpandey5 <cpandey5@avaya.com>
2020-05-02 19:04:31 +0530

    added more url

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,7 +13,23 @@ public class Application {
     public String home() {
         return "Hello Chandan Docker World";
     }
+    @RequestMapping("/test")
+    public String hometest() throws InterruptedException {
+        Thread.sleep(1000);
+        return "slept for a sec";
+    }
+    @RequestMapping("/heavy")
+    public String homecpu() throws InterruptedException {
+        long start=System.currentTimeMillis();
+        for (long i = 0; i < 2000000000; i++) {
 
+        }
+        for (long i = 0; i < 2000000000; i++) {
+
+        }
+        long end=System.currentTimeMillis();
+        return "some CPU work ...done in "+(start-end)+" milli secs";
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
